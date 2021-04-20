6d60e3dee97b603f4f05b72bf5a0e110344cb0f5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-20 23:06:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,8 +25,12 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     if params[:operation] == "add"
       params[:number1].to_i + params[:number2].to_i
-
-    binding.pry
-  end
+    elsif params[:operation] == "subract"
+      params[:number1].to_i - params[:number2].to_i
+    elsif params[:operation] == "multiply"
+      params[:number1].to_i * params[:number2].to_i
+    else
+      params[:number1].to_i & params[:number2].to_i
+    end
   end
 end
