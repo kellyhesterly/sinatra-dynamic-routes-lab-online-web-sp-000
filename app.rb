eec70ca59b8062ec4883b230adc6c4fc9dc9656e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-20 22:58:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @say = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
     @say.each do |word|
-      full_phrase = word.split(", ")
+      full_phrase = word.split(" ")
     end
   end
 end
