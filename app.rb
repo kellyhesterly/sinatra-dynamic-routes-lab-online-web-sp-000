d28ba1852c5e87bab165319a60452782276e48a9
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-20 22:47:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,4 +12,9 @@ class App < Sinatra::Base
     square_number = @number.to_i * @number.to_i
     "#{square_number}"
   end
+
+  get '/say/:number/:phrase' do
+    params[:phrase]
+    binding.pry
+  end
 end
